788cdb9ad36d97324ade99ef7794d639c95278a0
Toni Galata <tonigalata@Tonis-MacBook-Pro.local>
2020-03-31 20:03:07 -0600

    userauth changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,23 @@ import { Container } from 'semantic-ui-react';
 import Home from './components/shared/Home';
 import NoMatch from './components/shared/NoMatch';
 import Navbar from './components/shared/Navbar';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import FetchUser from './components/auth/FetchUser';
 
 const App = () => (
   <>
     <Navbar />
-    <Container>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route component={NoMatch} />
-      </Switch>
-    </Container>
+    <FetchUser>
+      <Container>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Container>
+    </FetchUser>
   </>
 )
 export default App;
